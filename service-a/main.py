c75109eed73f2ee427aa6c30a6dbb8287b66af25
vasilymarchenko <vam@sitecore.net>
2025-06-30 18:37:29 +0300

    Reworked internal routing

--- service-a/main.py
+++ service-a/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 import httpx
+import os
 
 app = FastAPI()
 
 @app.get("/call-b")
 async def call_b():
+    service_b_url = os.environ.get("SERVICE_B_URL")
     async with httpx.AsyncClient() as client:
-        r = await client.get("http://service-b:8000/ping")
+        r = await client.get(f"{service_b_url}/ping")
         return {"from-b": r.json()}
